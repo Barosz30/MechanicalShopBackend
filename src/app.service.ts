bffc02541b3e5816361e49901903c9e3c0a81a97
Mirosław Wandyk <barosz1111@gmail.com>
2026-03-04 09:48:43 +0100

    new message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (
+      'go to https://mechanicalshopbackend.onrender.com/graphql for graphql routes\n' +
+      'go to https://mechanicalshopbackend.onrender.com/api for swagger'
+    );
   }
 }
