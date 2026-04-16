f782f99a85eeacd35dacbea40d69dce95cb03556
Mirosław Wandyk <barosz1111@gmail.com>
2026-04-16 15:40:42 +0200

    mutations fix

--- src/shop-items/shop-items.module.ts
+++ src/shop-items/shop-items.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShopItem } from './entities/shop-item.entity';
 import { ShopItemDetails } from './entities/shop-item-details.entity';
 import { Category } from '../categories/entities/category.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ShopItem, ShopItemDetails, Category])],
+  imports: [TypeOrmModule.forFeature([ShopItem, ShopItemDetails, Category]), AuthModule],
   providers: [ShopItemsResolver, ShopItemsService],
 })
 export class ShopItemsModule {}
